c934e40a7a4b97920a19b53076054bd638bd77b3
Acaicia <628cjh@gmail.com>
2024-03-07 02:32:16 -0600

    dev

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import datetime
 import json
 import os
 import threading
-import threading
 
 DBusGMainLoop(set_as_default=True)
-bluez_hci0_path = "/org/bluez/hci0/"
 bus = dbus.SystemBus()
+bluez_hci0_path = "/org/bluez/hci0/"
 
 def get_permitted_devices():
     permitted_devices_file = "permitted_devices.json"
@@ -27,48 +26,51 @@ def get_permitted_devices():
 
 class BluetoothManager:
     def __init__(self, device):
+        self.bus = dbus.SystemBus()
         self.device_name = device["name"]
         self.device_address = device["mac_address"]
         self.device_path = device["device_path"]
 
-    def on_properties_changed(self, interface, changed_properties, invalidated_properties, path=None, **kwargs):
-        print(kwargs)
-        if path != self.device_path:
-            return
-
+    def on_properties_changed(self, interface, changed_properties, invalidated_properties, path=None):
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
         for property_name, value in changed_properties.items():
             if property_name == "Volume":
-                print(f"[{timestamp}] Volume Changed: {value}")
+                print(f"[{timestamp}] {self.device_name} Volume Changed: {value} on path: {path}")
             elif property_name == "Status":
-                print(f"[{timestamp}] Playback Status Changed: {value}")
+                print(f"[{timestamp}] {self.device_name} Playback Status Changed: {value}")
             elif property_name == "Track":
                 title = value.get('Title', 'Unknown Title')
                 artist = value.get('Artist', 'Unknown Artist')
-                print(f"[{timestamp}] Now Playing: {title} by {artist}")
+                print(f"[{timestamp}] {self.device_name} Now Playing: {title} by {artist}")
+
+        if "Connected" in changed_properties:
+            connected = changed_properties["Connected"]
+            status = "connected" if connected else "disconnected"
+            print(f"[{timestamp}] {self.device_name} {status}: {path}")
 
     def on_device_discovered(self, object_path, interfaces_added):
         if self.device_address in object_path:
             print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {self.device_name} Device detected in range: {self.device_address}")
-            GLib.idle_add(self.attempt_connect)
+            GLib.idle_add(self.attempt_reconnect)
 
-    def attempt_connect(self):
+    def attempt_reconnect(self):
         if not self.is_device_connected():
-            print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {self.device_name} Attempting to connect to {self.device_address}")
+            print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {self.device_name} Attempting to reconnect to {self.device_address}")
             try:
-                device_proxy = bus.get_object("org.bluez", self.device_path)
+                device_proxy = self.bus.get_object("org.bluez", self.device_path)
                 device_interface = dbus.Interface(device_proxy, "org.bluez.Device1")
                 device_interface.Connect()
-                print("Connected successfully.")
+                print("Reconnected successfully.")
             except dbus.exceptions.DBusException as e:
-                print(f"Connection failed: {e}")
+                print(f"Reconnection failed: {e}")
         else:
                 print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {self.device_name} ({self.device_address}) is already connected.")
         return False
 
     def is_device_connected(self):
         try:
-            device_proxy = bus.get_object("org.bluez", self.device_path)
+            device_proxy = self.bus.get_object("org.bluez", self.device_path)
             device_properties = dbus.Interface(device_proxy, dbus.PROPERTIES_IFACE)
             return device_properties.Get("org.bluez.Device1", "Connected")
         except dbus.DBusException as e:
@@ -76,10 +78,12 @@ class BluetoothManager:
             return False
 
     def main(self):
-        if not self.is_device_connected():
-            self.attempt_connect()
+        self.bus.add_signal_receiver(self.on_properties_changed,
+                                     dbus_interface="org.freedesktop.DBus.Properties",
+                                     signal_name="PropertiesChanged",
+                                     path_keyword="path")
 
-        bus.add_signal_receiver(self.on_device_discovered,
+        self.bus.add_signal_receiver(self.on_device_discovered,
                                      dbus_interface="org.freedesktop.DBus.ObjectManager",
                                      signal_name="InterfacesAdded")
 
@@ -92,29 +96,6 @@ class BluetoothManager:
             loop.quit()
 
 if __name__ == "__main__":
-    devices = get_permitted_devices()
-
-    print("Permitted Devices:")
-    for device in devices:
-        print(f"Device: {device['name']} ({device['mac_address']}) device_path: {device['device_path']}")
-
-
-    managers = [BluetoothManager(device) for device in devices]
-
-    bus.add_signal_receiver(
-        lambda *args, **kwargs: [manager.on_properties_changed(*args, **kwargs) for manager in managers if kwargs.get('path') == manager.device_path],
-        dbus_interface="org.freedesktop.DBus.Properties",
-        signal_name="PropertiesChanged",
-        arg0="org.bluez.MediaPlayer1",
-        path_keyword="path"
-    )
-
-    threads = []
-    for device in devices:
+    for device in get_permitted_devices():
         bluetooth_manager = BluetoothManager(device)
-        thread = threading.Thread(target=bluetooth_manager.main)
-        threads.append(thread)
-        thread.start()
-
-    for thread in threads:
-        thread.join()
+        bluetooth_manager.main()
