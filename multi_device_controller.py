f1613a9561c83f872a192f2e1ba5ebc2ca92f540
Acaicia <628cjh@gmail.com>
2024-03-08 02:58:43 -0600

    dev

--- multi_device_controller.py
+++ multi_device_controller.py
@@ -9,7 +9,7 @@ def get_devices():
     default_file = 'devices.json'
     parser = argparse.ArgumentParser()
     parser.add_argument('--file', help='Specify the JSON file to open', default=default_file)
-    parser.add_argument('-h', '--help', action='help', help='Show this help message and exit')
+    parser.add_argument('-h', action='help', help='Show this help message and exit')
     args = parser.parse_args()
     file_path = args.file
 
