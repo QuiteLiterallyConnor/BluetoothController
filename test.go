6be7038dd3aa75fc108547b66bd61dd3c947b855
Acaicia <628cjh@gmail.com>
2024-03-16 20:49:02 -0500

    DEV

--- test.go
+++ test.go
@@ -1,73 +1,31 @@
 package main
 
 import (
-    "context"
-    "flag"
-    "fmt"
-    "os"
-    "strings"
-    // "time"
+	"fmt"
+	"os"
 
-    "github.com/godbus/dbus/v5"
+	"github.com/godbus/dbus/v5"
 )
-
-var (
-    deviceName string
-    deviceMAC  string
-)
-
-func init() {
-    flag.StringVar(&deviceName, "name", "", "Name of the Bluetooth device")
-    flag.StringVar(&deviceMAC, "mac_address", "", "MAC address of the Bluetooth device")
-    flag.Parse()
-
-    if deviceName == "" || deviceMAC == "" {
-        fmt.Println("Both device name and MAC address must be specified.")
-        flag.PrintDefaults()
-        os.Exit(1)
-    }
-}
-
-func listenForPropertiesChanged(conn *dbus.Conn, devicePath string) {
-    ctx, cancel := context.WithCancel(context.Background())
-    defer cancel()
-
-    matchSignal := fmt.Sprintf("type='signal',interface='org.freedesktop.DBus.Properties',path='%s'", devicePath)
-    conn.BusObject().CallWithContext(ctx, "org.freedesktop.DBus.AddMatch", 0, matchSignal)
-
-    c := make(chan *dbus.Signal, 10)
-    conn.Signal(c)
-
-    for {
-        select {
-        case v := <-c:
-            handlePropertiesChanged(v)
-        case <-ctx.Done():
-            return
-        }
-    }
-}
-
-func handlePropertiesChanged(signal *dbus.Signal) {
-    // Implementation similar to the Python version's on_properties_changed
-    // Extract and process properties from the signal.Body
-    fmt.Println("PropertiesChanged signal received:", signal)
-    // Note: You'll need to adjust the extraction logic based on the signal's actual structure.
-}
 
 func main() {
-    deviceMACFormatted := strings.ToUpper(strings.Replace(deviceMAC, ":", "_", -1))
-    devicePath := fmt.Sprintf("/org/bluez/hci0/dev_%s", deviceMACFormatted)
+	conn, err := dbus.ConnectSessionBus()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to connect to session bus:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
 
-    conn, err := dbus.SystemBus()
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to connect to SystemBus: %s\n", err)
-        os.Exit(1)
-    }
+	if err = conn.AddMatchSignal(
+		dbus.WithMatchObjectPath("/org/freedesktop/DBus"),
+		dbus.WithMatchInterface("org.freedesktop.DBus"),
+		dbus.WithMatchSender("org.freedesktop.DBus"),
+	); err != nil {
+		panic(err)
+	}
 
-    go listenForPropertiesChanged(conn, devicePath)
-
-    // Keep the main goroutine running to listen for signals
-    fmt.Println("Listening for property changes. Press CTRL+C to exit.")
-    <-make(chan struct{})
+	c := make(chan *dbus.Signal, 10)
+	conn.Signal(c)
+	for v := range c {
+		fmt.Println(v)
+	}
 }
