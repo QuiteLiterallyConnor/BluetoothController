f4d95b764b607e24611735879ace7ecfbac5f432
Acaicia <628cjh@gmail.com>
2024-03-16 21:04:27 -0500

    dev

--- test.go
+++ test.go
@@ -1,81 +1,85 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"strings"
+    "bufio"
+    "flag"
+    "fmt"
+    "os"
+    "strings"
+    "sync"
 
-	"github.com/godbus/dbus/v5"
+    "github.com/godbus/dbus/v5"
 )
 
 var (
-	deviceName string
-	deviceMAC  string
+    deviceName string
+    deviceMAC  string
 )
 
 func init() {
-	flag.StringVar(&deviceName, "name", "", "Name of the Bluetooth device")
-	flag.StringVar(&deviceMAC, "mac_address", "", "MAC address of the Bluetooth device")
-	flag.Parse()
+    flag.StringVar(&deviceName, "name", "", "Name of the Bluetooth device")
+    flag.StringVar(&deviceMAC, "mac_address", "", "MAC address of the Bluetooth device")
+    flag.Parse()
 
-	if deviceName == "" || deviceMAC == "" {
-		fmt.Println("Both device name and MAC address must be specified using the -name and -mac_address flags.")
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+    if deviceName == "" || deviceMAC == "" {
+        fmt.Println("Both device name and MAC address must be specified using the -name and -mac_address flags.")
+        flag.PrintDefaults()
+        os.Exit(1)
+    }
 }
 
 func main() {
-	var action string
+    deviceMACFormatted := strings.ToUpper(strings.Replace(deviceMAC, ":", "_", -1))
+    mediaPlayerPath := fmt.Sprintf("/org/bluez/hci0/dev_%s/player0", deviceMACFormatted)
 
-	fmt.Print("Enter the action to perform (play, pause, next, previous): ")
-	fmt.Scanln(&action)
+    conn, err := dbus.SystemBus()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Failed to connect to SystemBus: %s\n", err)
+        os.Exit(1)
+    }
 
-	deviceMACFormatted := strings.ToUpper(strings.Replace(deviceMAC, ":", "_", -1))
-	mediaPlayerPath := fmt.Sprintf("/org/bluez/hci0/dev_%s/player0", deviceMACFormatted)
+    var wg sync.WaitGroup
+    wg.Add(1)
 
-	conn, err := dbus.SystemBus()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to connect to SystemBus: %s\n", err)
-		os.Exit(1)
-	}
+    go listenForPropertiesChanged(conn, mediaPlayerPath, &wg)
 
-	switch action {
-	case "play", "pause", "next", "previous":
-		controlMedia(conn, mediaPlayerPath, strings.Title(action))
-	default:
-		fmt.Println("Invalid action. Use 'play', 'pause', 'next', or 'previous'")
-	}
+    // Replace the previous action prompt with a loop that allows continuous command input
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        fmt.Print("Enter command (play, pause, next, previous, quit): ")
+        command, _ := reader.ReadString('\n')
+        command = strings.TrimSpace(command)
 
-	listenForPropertiesChanged(conn, mediaPlayerPath)
+        if command == "quit" {
+            fmt.Println("Exiting...")
+            break
+        }
 
-	// Block the main goroutine to keep listening for property changes.
-	fmt.Println("Press CTRL+C to exit.")
-	select {}
+        switch command {
+        case "play", "pause", "next", "previous":
+            controlMedia(conn, mediaPlayerPath, strings.Title(command))
+        default:
+            fmt.Println("Invalid command. Use 'play', 'pause', 'next', 'previous', or 'quit'")
+        }
+    }
+
+    wg.Wait() // Wait for the listener goroutine to finish
 }
 
-func controlMedia(conn *dbus.Conn, mediaPlayerPath, method string) {
-	mediaPlayer := conn.Object("org.bluez", dbus.ObjectPath(mediaPlayerPath))
-	call := mediaPlayer.Call("org.bluez.MediaPlayer1."+method, 0)
-	if call.Err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to %s: %s\n", strings.ToLower(method), call.Err)
-		return
-	}
-	fmt.Printf("%s action executed for %s\n", method, deviceName)
-}
+// Updated controlMedia function as previously defined
 
-func listenForPropertiesChanged(conn *dbus.Conn, mediaPlayerPath string) {
-	matchRule := fmt.Sprintf("type='signal',interface='org.freedesktop.DBus.Properties',path='%s'", mediaPlayerPath)
-	conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0, matchRule)
+// Updated listenForPropertiesChanged function with a WaitGroup parameter
+func listenForPropertiesChanged(conn *dbus.Conn, mediaPlayerPath string, wg *sync.WaitGroup) {
+    defer wg.Done()
 
-	c := make(chan *dbus.Signal, 10)
-	conn.Signal(c)
+    matchRule := fmt.Sprintf("type='signal',interface='org.freedesktop.DBus.Properties',path='%s'", mediaPlayerPath)
+    conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0, matchRule)
 
-	go func() {
-		for v := range c {
-			fmt.Println("PropertiesChanged signal received:", v)
-			// You can further process the signal here as needed.
-		}
-	}()
+    c := make(chan *dbus.Signal, 10)
+    conn.Signal(c)
+
+    for v := range c {
+        // Process the signal here
+        // Break loop or return if a "quit" condition is met, if necessary
+    }
 }
