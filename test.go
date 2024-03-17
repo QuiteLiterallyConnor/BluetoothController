17e4e406930a5218afc1d6430f2d066cd93b3d2d
Acaicia <628cjh@gmail.com>
2024-03-16 20:44:44 -0500

    dev

--- test.go
+++ test.go
@@ -1,85 +1,73 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"strings"
-	"time"
+    "context"
+    "flag"
+    "fmt"
+    "os"
+    "strings"
+    "time"
 
-	"github.com/godbus/dbus/v5"
+    "github.com/godbus/dbus/v5"
 )
 
 var (
-	deviceName string
-	deviceMAC  string
+    deviceName string
+    deviceMAC  string
 )
 
 func init() {
-	flag.StringVar(&deviceName, "name", "", "Name of the Bluetooth device")
-	flag.StringVar(&deviceMAC, "mac_address", "", "MAC address of the Bluetooth device")
-	flag.Parse()
+    flag.StringVar(&deviceName, "name", "", "Name of the Bluetooth device")
+    flag.StringVar(&deviceMAC, "mac_address", "", "MAC address of the Bluetooth device")
+    flag.Parse()
 
-	if deviceName == "" || deviceMAC == "" {
-		fmt.Println("Both device name and MAC address must be specified using the -name and -mac_address flags.")
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+    if deviceName == "" || deviceMAC == "" {
+        fmt.Println("Both device name and MAC address must be specified.")
+        flag.PrintDefaults()
+        os.Exit(1)
+    }
 }
 
-func main() {
-	deviceMACFormatted := strings.ToUpper(strings.Replace(deviceMAC, ":", "_", -1))
-	mediaPlayerPath := fmt.Sprintf("/org/bluez/hci0/dev_%s", deviceMACFormatted)
+func listenForPropertiesChanged(conn *dbus.Conn, devicePath string) {
+    ctx, cancel := context.WithCancel(context.Background())
+    defer cancel()
 
-	conn, err := dbus.SystemBus()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to connect to SystemBus: %s\n", err)
-		os.Exit(1)
-	}
+    matchSignal := fmt.Sprintf("type='signal',interface='org.freedesktop.DBus.Properties',path='%s'", devicePath)
+    conn.BusObject().CallWithContext(ctx, "org.freedesktop.DBus.AddMatch", 0, matchSignal)
 
-	conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0,
-		"type='signal',interface='org.freedesktop.DBus.Properties',path='"+mediaPlayerPath+"',member='PropertiesChanged'")
+    c := make(chan *dbus.Signal, 10)
+    conn.Signal(c)
 
-	c := make(chan *dbus.Signal, 10)
-	conn.Signal(c)
-
-	fmt.Println("Listening for property changes...")
-	for v := range c {
-		fmt.Printf("OMG WE GOT A SIGNAL (PROPERTY CHANGE!!!) :) :) :)\n")
-		handlePropertiesChanged(v)
-	}
+    for {
+        select {
+        case v := <-c:
+            handlePropertiesChanged(v)
+        case <-ctx.Done():
+            return
+        }
+    }
 }
 
 func handlePropertiesChanged(signal *dbus.Signal) {
-	if len(signal.Body) < 3 {
-		return
-	}
+    // Implementation similar to the Python version's on_properties_changed
+    // Extract and process properties from the signal.Body
+    fmt.Println("PropertiesChanged signal received:", signal)
+    // Note: You'll need to adjust the extraction logic based on the signal's actual structure.
+}
 
-	interfaceName, ok := signal.Body[0].(string)
-	if !ok || interfaceName != "org.bluez.MediaPlayer1" {
-		return
-	}
+func main() {
+    deviceMACFormatted := strings.ToUpper(strings.Replace(deviceMAC, ":", "_", -1))
+    devicePath := fmt.Sprintf("/org/bluez/hci0/dev_%s", deviceMACFormatted)
 
-	changedProperties, ok := signal.Body[1].(map[string]dbus.Variant)
-	if !ok {
-		return
-	}
+    conn, err := dbus.SystemBus()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Failed to connect to SystemBus: %s\n", err)
+        os.Exit(1)
+    }
 
-	timestamp := time.Now().Format("2006-01-02 15:04:05")
-	for property, value := range changedProperties {
-		switch property {
-		case "Volume":
-			fmt.Printf("[%s] %s Volume Changed: %v\n", timestamp, deviceName, value.Value())
-		case "Status":
-			fmt.Printf("[%s] %s Playback Status Changed: %v\n", timestamp, deviceName, value.Value())
-		case "Track":
-			trackInfo, ok := value.Value().(map[string]dbus.Variant)
-			if !ok {
-				return
-			}
-			title, _ := trackInfo["Title"].Value().(string)
-			artist, _ := trackInfo["Artist"].Value().(string)
-			fmt.Printf("[%s] %s Now Playing: %s by %s\n", timestamp, deviceName, title, artist)
-		}
-	}
+    go listenForPropertiesChanged(conn, devicePath)
+
+    // Keep the main goroutine running to listen for signals
+    fmt.Println("Listening for property changes. Press CTRL+C to exit.")
+    <-make(chan struct{})
 }
