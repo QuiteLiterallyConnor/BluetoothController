a267b99b4654cf756589a52a8dfcd0b15adbb2dc
Acaicia <628cjh@gmail.com>
2024-03-16 19:15:40 -0500

    dev

--- device_controller.py
+++ device_controller.py
@@ -2,14 +2,10 @@ import argparse
 from src.bluetooth_manager import BluetoothManager
 
 def get_device():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--name", help="Name of the device")
-    parser.add_argument("--mac_address", help="MAC address of the device")
+    parser = argparse.ArgumentParser(description="Control a Bluetooth device by providing its name (--name) and MAC address (--mac_address).")
+    parser.add_argument("--name", help="Name of the device", required=True)
+    parser.add_argument("--mac_address", help="MAC address of the device", required=True)
     args = parser.parse_args()
-
-    if args.help:
-        parser.print_help()
-        exit()
 
     device = {
         "name": args.name,
